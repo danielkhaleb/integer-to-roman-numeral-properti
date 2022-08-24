5473f1037afc72210f9c79359a5f4ec9c9a32ebf
Daniel Khaleb <daniel.khaleb@gmail.com>
2022-08-24 01:14:11 +0100

    build: map helper folder to test files from helper folder

--- jest.config.js
+++ jest.config.js
@@ -7,11 +7,12 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     // Handle module aliases (this will be automatically configured for you soon)
     '^@/components/(.*)$': '<rootDir>/components/$1',
-
+    "^helpers(.*)$": "<rootDir>/helpers/$1",
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
